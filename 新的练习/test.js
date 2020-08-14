43405a69b95c76fd063e4b2ae0233e1a38d60437
suqi04 <suqi04@baidu.com>
2020-08-14 15:07:52 +0800

    bootstrap 后台模板

---
+++ 新的练习/test.js
@@ -0,0 +1,12 @@
+var obj = {
+    key1:'value1',
+    key2:'value2',
+    key3:'value3',
+    key4:'value4',
+}
+
+// for((key,value) in obj){
+//     console.log(value);
+// }
+
+console.log({key1:'value1',key2:'value2'}.key1)
